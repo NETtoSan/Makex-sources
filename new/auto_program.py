7f427795a199a3f1ccae05ee35458c72b3a8a53a
NETtoSan <netzamora05@gmail.com>
2022-08-26 19:08:21 +0700

    A TODO programs for tonight

---
+++ new/auto_program.py
@@ -0,0 +1,8 @@
+import cyberpi
+import time
+import gamepad
+import mbot2
+import event
+import mbuild_modules.starter_shield as starter_shield
+
+# Optimize code tonight. Source: ../auto_program_new.py
