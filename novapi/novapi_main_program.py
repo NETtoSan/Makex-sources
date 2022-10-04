a13e09820c9e3c093ec3660438e0be8217f3d6e8
NETtoSan <netzamora05@gmail.com>
2022-10-04 19:41:18 +0700

    h

--- novapi/novapi_main_program.py
+++ novapi/novapi_main_program.py
@@ -24,10 +24,14 @@ def LoadMe():
 def Moving():
     # Code for bot rotation. Suppose Rx is turning your joystick left to right
 
-    motor_front_1 = 0.8 * (gamepad.get_joystick("Lx") + gamepad.get_joystick("Rx"))
-    motor_front_2 = -0.8 * (gamepad.get_joystick("Ly") - gamepad.get_joystick("Rx"))
-    motor_sides_1 = 0.8 * (gamepad.get_joystick("Lx") + gamepad.get_joystick("Rx"))
-    motor_sides_2 = -0.8 * (gamepad.get_joystick("Ly") - gamepad.get_joystick("Rx") )
+    motor_front_1 = 0.8 * (gamepad.get_joystick("Lx")
+                           + gamepad.get_joystick("Rx"))
+    motor_front_2 = -0.8 * (gamepad.get_joystick("Ly")
+                            - gamepad.get_joystick("Rx"))
+    motor_sides_1 = 0.8 * (gamepad.get_joystick("Lx")
+                           + gamepad.get_joystick("Rx"))
+    motor_sides_2 = -0.8 * (gamepad.get_joystick("Ly")
+                            - gamepad.get_joystick("Rx"))
 
     encoder_motor_M1.set_power(motor_front_1)
     encoder_motor_M2.set_power(motor_sides_1)
