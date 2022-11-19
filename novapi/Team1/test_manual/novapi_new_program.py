65b9d803fccbd9fe02d825d1646784aa5a78b991
NETtoSan <netzamora05@gmail.com>
2022-11-19 14:43:30 +0700

    fuse auto code into main

--- novapi/Team1/test_manual/novapi_new_program.py
+++ novapi/Team1/test_manual/novapi_new_program.py
@@ -40,12 +40,22 @@ encoder_motor_M1 = encoder_motor_class("M1", "INDEX1")
 encoder_motor_M2 = encoder_motor_class("M2", "INDEX1")
 encoder_motor_M3 = encoder_motor_class("M3", "INDEX1")
 encoder_motor_M4 = encoder_motor_class("M4", "INDEX1")
+# Auto motors
+motor1 = encoder_motor_class("M1", "INDEX1")
+motor2 = encoder_motor_class("M2", "INDEX1")
+motor3 = encoder_motor_class("M3", "INDEX1")
+motor4 = encoder_motor_class("M4", "INDEX1")
 
 
 def AutoStart():
     global auto_stage
+    dual_rgb_sensor_1.set_led_color("blue")
+    while not gamepad.is_key_pressed("L1"):
+        pass
+        
+    AutoAssets.ShootRoutine()
+    time.sleep(5)
     pass
-
 
 def Manual():
     global auto_stage
@@ -143,10 +153,6 @@ def LoadMe():
 
     smartservo_arm.set_power(50)
     smartservo_arm.move(30, 10)
-    # power_expand_board.set_power("BL1", 50)
-    # power_expand_board.set_power(feeddc_main, 50)
-    # power_expand_board.stop("BL1")
-    # power_expand_board.stop(feeddc_main)
 
 
 class JoyRes:
@@ -315,6 +321,112 @@ class ManualRes:
         encoder_motor_M3.set_power(0)
         encoder_motor_M4.set_power(0)
 
+
+class MovementAsset:
+    def __init__(self):
+        pass
+
+    def move(v1, v2, v3, v4):
+        motor1.set_power(v1)
+        motor2.set_power(v2)
+        motor3.set_power(v3)
+        motor4.set_power(v4)
+
+    def stop():
+        motor1.set_power(0)
+        motor2.set_power(0)
+        motor3.set_power(0)
+        motor4.set_power(0)
+
+
+class AutoAssets:
+    def __init__(self):
+        pass
+
+    def MoveForward():
+        MovementAsset.move(25, -25, 25, -25)
+        pass
+
+    def MoveBackward():
+        MovementAsset.move(-25, 25, -25, 25)
+        pass
+
+    def RotateLeft():
+        MovementAsset.move(-50, -50, -50, -50)
+        pass
+
+    def RotateRight():
+        MovementAsset.move(50, 50, 50, 50)
+        pass
+    def StopMoving():
+        MovementAsset.move(0, 0, 0, 0)
+    def Shoot():
+        power_expand_board.set_power("DC3", 100)
+        pass
+
+    # Return value functions
+
+    def getSelfAngle():
+
+        angle = [novapi.get_pitch(), novapi.get_roll(), novapi.get_yaw()]
+        return angle
+
+
+    def GetDistance():
+        # GetDistance utilizes a ranging sensor Module
+        # What it does is detects an object in front of it
+        # And returns a distance value as number
+
+        range = distance_sensor_1.get_distance()
+
+        return range
+
+    # Presets
+
+    def ShootRoutine():
+        dual_rgb_sensor_1.set_led_color("red")
+        dual_rgb_sensor_2.set_led_color("red")
+        time.sleep(1)    
+
+        power_expand_board.set_power("DC2",70)
+        power_expand_board.set_power("DC1",-100)
+        
+        AutoAssets.MoveForward()
+        time.sleep(0.5)
+        AutoAssets.RotateLeft()
+        time.sleep(0.25)
+
+        AutoAssets.MoveForward()
+        time.sleep(2)
+        AutoAssets.RotateRight()
+        time.sleep(0.2)
+        
+        AutoAssets.MoveForward()
+        power_expand_board.set_power("BL1",100)
+        power_expand_board.set_power("BL2",100)
+        time.sleep(1.25)
+
+        AutoAssets.StopMoving()
+        dual_rgb_sensor_2.set_led_color("green")
+
+        time.sleep(10)
+        for i in range(10):
+            dual_rgb_sensor_1.set_led_color("blue")
+            AutoAssets.RotateLeft()
+            time.sleep(0.1)
+            AutoAssets.StopMoving()
+            dual_rgb_sensor_1.set_led_color("red")
+            time.sleep(0.2)
+
+        pass
+
+    def EmbraceBallRoutine():
+
+        pass
+
+    def GrabCubeRoutine():
+
+        pass
 
 auto_stage = 1
 while True:
