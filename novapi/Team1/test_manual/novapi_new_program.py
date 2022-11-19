a912eb0d91d5eeab25fa5063358552e7d86b4880
NETtoSan <netzamora05@gmail.com>
2022-11-20 01:46:25 +0700

    invert hand control direction

--- novapi/Team1/test_manual/novapi_new_program.py
+++ novapi/Team1/test_manual/novapi_new_program.py
@@ -253,8 +253,8 @@ class JoyRes:
         pass
 
     def HandControl():
-        power_expand_board.set_power(handdc1, gamepad.get_joystick("Ry"))
-        power_expand_board.set_power(handdc2, gamepad.get_joystick("Ry"))
+        power_expand_board.set_power(handdc1, - (gamepad.get_joystick("Ry")))
+        power_expand_board.set_power(handdc2, - (gamepad.get_joystick("Ry")))
         # smartservo_arm.move(gamepad.get_joystick("Ry"), 10)
         pass
 
