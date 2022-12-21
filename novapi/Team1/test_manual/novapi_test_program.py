d43eb1729bb98c39fbc1a18839483c2e3f3d3e80
NETtoSan <netzamora05@gmail.com>
2022-12-21 16:25:02 +0700

    test program

--- novapi/Team1/test_manual/novapi_test_program.py
+++ novapi/Team1/test_manual/novapi_test_program.py
@@ -11,39 +11,55 @@ from mbuild.encoder_motor import encoder_motor_class
 # initialize variables
 BL_SPEED = 0
 ranging_data = [] # Each data are 5 degrees apart
-
+vl = 0.7
 
 # new class
-smartservo_1 = smartservo_class("M2", "INDEX1")
-smartservo_2 = smartservo_class("M2", "INDEX2")
-smartservo_3 = smartservo_class("M4", "INDEX1")
-distance_sensor_1 = ranging_sensor_class("PORT3", "INDEX1")
-dual_rgb_sensor_1 = dual_rgb_sensor_class("PORT2", "INDEX1")
-encoder_motor_M1 = encoder_motor_class("M1", "INDEX1")
+smartservo_1 = smartservo_class("M6", "INDEX1")
+smartservo_2 = smartservo_class("M6", "INDEX2")
+smartservo_3 = smartservo_class("M5", "INDEX1")
+distance_sensor_1 = ranging_sensor_class("PORT5", "INDEX1")
+dual_rgb_sensor_1 = dual_rgb_sensor_class("PORT4", "INDEX1")
 
-BL_SPEED = 50
+# Encoder motors
+EFl = encoder_motor_class("M1", "INDEX1")
+EFr = encoder_motor_class("M2", "INDEX1")
+ERl = encoder_motor_class("M3", "INDEX1")
+ERr = encoder_motor_class("M4", "INDEX1")
 
-while True:
-    time.sleep(0.001)
+def RangingLED():
     if distance_sensor_1.get_distance() > 200:
         dual_rgb_sensor_1.set_led_color("green")
 
     if distance_sensor_1.get_distance() < 200:
         dual_rgb_sensor_1.set_led_color("blue")
+def MovementCode():
+    global vl
+    Lx = gamepad.get_joystick("Lx")
+    Ly = gamepad.get_joystick("Ly")
 
-    if gamepad.is_key_pressed("L2"):
-        smartservo_3.move(90, 50)
-        time.sleep(1)
-        steps = 36
-        while steps <= 36:
-            smartservo_3.move(-5)
-            steps += 1
-            time.sleep(0.25)
+    EFl.set_power(vl * (Ly - Lx))
+    EFr.set_power(vl * (-Ly - Lx))
+    ERl.set_power(-vl * (-Ly + Lx))
+    ERr.set_power(-vl * (Ly + Lx))
+    
+def Manual():
+    while True:
+        time.sleep(0.001)
+        RangingLED()
+        MovementCode()
 
+        if gamepad.is_key_pressed("L2"):
+            smartservo_3.move(90, 50)
+            time.sleep(1)
+            steps = 180
+            while steps <= 180:
+                smartservo_3.move(-5)
+                steps += 1
+                time.sleep(0.25)
 
-        smartservo_3.move(90,50)
-        while not not gamepad.is_key_pressed("L2"):
-            pass
+            smartservo_3.move(90,50)
+            while not not gamepad.is_key_pressed("L2"):
+                pass
 
 
         #smartservo_3.move(-180, 50)
@@ -52,29 +68,22 @@ while True:
         #time.sleep(1)
 
     
-    if gamepad.is_key_pressed("N1"):
-        if BL_SPEED is 50:
-            BL_SPEED = 100
-        elif BL_SPEED is 100:
-            BL_SPEED = 0
-        else:
-            BL_SPEED = 50
-        while not not gamepad.is_key_pressed("N1"):
-            pass
+        if gamepad.is_key_pressed("R1"):
+            if BL_SPEED is 50:
+                BL_SPEED = 100
+            elif BL_SPEED is 100:
+                BL_SPEED = 0
+            else:
+                BL_SPEED = 50
+            while not not gamepad.is_key_pressed("R1"):
+                pass
     
 
-    if gamepad.is_key_pressed("R1"):
+
         power_expand_board.set_power("BL1", BL_SPEED)
         power_expand_board.set_power("BL2", BL_SPEED)
 
+        smartservo_1.set_power(0.1 * gamepad.get_joystick("Rx"))
+        smartservo_2.move(0.1 * gamepad.get_joystick("Ry"), 0.1 * 0)
 
-    # Magazine code
-    if gamepad.is_key_pressed("Up"):
-        encoder_motor_M1.set_power(20)
-
-    else:
-        encoder_motor_M1.set_power(0)
-
-    smartservo_1.move(0.3 * gamepad.get_joystick("Rx"), 0.3 * gamepad.get_joystick("Rx"))
-    smartservo_2.move(0.1 * gamepad.get_joystick("Ly"), 0.1 * gamepad.get_joystick("Ly"))
-
+Manual()
