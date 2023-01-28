681f6bf52d410665cb44416fd23d820e12b29443
NETtoSan <netzamora05@gmail.com>
2023-01-29 02:48:19 +0700

    rename dc variables

--- novapi/Team1/test_manual/novapi_main_program.py
+++ novapi/Team1/test_manual/novapi_main_program.py
@@ -16,27 +16,21 @@ bp = 50
 vl = 0.5
 
 # DC motors
-dc1_variable = "DC1"
-dc2_variable = "DC2"
+dc_gun = "DC1"
+dc_front = "DC2"
 dc3_variable = "DC3"
-dc4_variable = "DC4"
+dc_hand = "DC4"
 dc5_variable = "DC5"
 
 # Sensors
 dual_rgb_sensor_1 = dual_rgb_sensor_class("PORT2", "INDEX1")
 dual_rgb_sensor_2 = dual_rgb_sensor_class("PORT2", "INDEX2")
 
-# Arm
-smartservo_arm = smartservo_class("M6", "INDEX1")
-
-# Turret
-smartservo_updown = smartservo_class("M5", "INDEX1")
-
 # Bot motors
-encoder_motor_M1 = encoder_motor_class("M1", "INDEX1")
-encoder_motor_M2 = encoder_motor_class("M2", "INDEX1")
-encoder_motor_M3 = encoder_motor_class("M3", "INDEX1")
-encoder_motor_M4 = encoder_motor_class("M4", "INDEX1")
+encode_fl = encoder_motor_class("M1", "INDEX1")
+encode_fr = encoder_motor_class("M2", "INDEX1")
+encode_rl = encoder_motor_class("M3", "INDEX1")
+encode_rr = encoder_motor_class("M4", "INDEX1")
 encode_feed = encoder_motor_class("M5", "INDEX1")
 
 
@@ -55,7 +49,7 @@ def Manual():
     LoadMe()
     while True:
         time.sleep(0.001)
-        JoyRes.MovingJoystick(invert)
+        JoyRes.MovingJoystick()
         ManualRes.InvertLED(invert)
         ManualRes.ControlLED(lrmode)
         JoyRes.MultiControl(lrmode, bp)
@@ -130,29 +124,21 @@ def Manual():
 
         # Dc feed
         if feeddc == 1:
-            power_expand_board.set_power(dc2_variable, -100)
+            power_expand_board.set_power(dc_front, -100)
         else:
-            power_expand_board.stop(dc2_variable)
+            power_expand_board.stop(dc_front)
 
 
 def LoadMe():
     global auto_stage
-
-    smartservo_arm.set_power(50)
-    smartservo_arm.move(30, 10)
-    # power_expand_board.set_power("BL1", 50)
-    # power_expand_board.set_power(dc1_variable, 50)
-    # power_expand_board.stop("BL1")
-    # power_expand_board.stop(dc1_variable)
 
 
 class JoyRes:
     def __init__(self):
         pass
 
-    def MovingJoystick(invert):
-        global auto_stage
-        global vl
+    def MovingJoystick():
+        global vl, invert
         # Code for bot movement from left to right.
         # If the bot starts slipping to unwanted direction. Tune variables here
         
@@ -182,29 +168,14 @@ class JoyRes:
                         - gamepad.get_joystick("Rx"))
             ERl = -vl * (gamepad.get_joystick("Ly") - Rr
                          + gamepad.get_joystick("Rx"))
-        encoder_motor_M1.set_power(EFl)
-        encoder_motor_M2.set_power(EFr)
-        encoder_motor_M3.set_power(ERl)
-        encoder_motor_M4.set_power(ERr)
+        encode_fl.set_power(EFl)
+        encode_fr.set_power(EFr)
+        encode_rl.set_power(ERl)
+        encode_rr.set_power(ERr)
 
     def TurretControl():
-        global auto_stage
-        # > 40 < 60
-        # if smartservo_updown.get_value("angle") > -43:
-        #    smartservo_updown.move(-1, 10)
-
-        # if smartservo_updown.get_value("angle") < -60:
-        #    smartservo_updown.move(1, 10)
-        # else:
-        if smartservo_updown.get_value("angle") < -56:
-            servo_value = smartservo_updown.get_value("angle")
-            while servo_value < -55:
-                smartservo_updown.move(3, 10)
-                servo_value = smartservo_updown.get_value("angle")
-
-        relative_angle = gamepad.get_joystick("Ry") / 3
-
-        smartservo_updown.move(relative_angle, 10)
+        global dc_gun
+        power_expand_board.set_power(dc_gun, gamepad.get_joystick("Ry") / 2)
 
     def FeedControl():
         if gamepad.is_key_pressed("L1"):
@@ -215,20 +186,21 @@ class JoyRes:
             encode_feed.set_power(0)
 
     def ShootControl():
+        global dc3_variable
         if gamepad.is_key_pressed("R1"):
             power_expand_board.set_power(dc3_variable, -100)
         else:
             power_expand_board.stop(dc3_variable)
 
     def GrabControl():
-        # DC4 L1 release R1 grab
+        global dc_hand
         if gamepad.is_key_pressed("L1"):
-            power_expand_board.set_power("DC4", 50)
+            power_expand_board.set_power(dc_hand, 50)
 
         elif gamepad.is_key_pressed("R1"):
-            power_expand_board.set_power("DC4", -50)
+            power_expand_board.set_power(dc_hand, -50)
         else:
-            power_expand_board.stop("DC4")
+            power_expand_board.stop(dc_hand)
         pass
 
     def HandControl():
@@ -275,38 +247,38 @@ class ManualRes:
 
     def MoveBackward():
         global auto_stage
-        encoder_motor_M1.set_power(-50)
-        encoder_motor_M2.set_power(50)
-        encoder_motor_M3.set_power(-50)
-        encoder_motor_M4.set_power(50)
+        encode_fl.set_power(-50)
+        encode_fr.set_power(50)
+        encode_rl.set_power(-50)
+        encode_rr.set_power(50)
 
     def MoveForward():
         global auto_stage
-        encoder_motor_M1.set_power(50)
-        encoder_motor_M2.set_power(-50)
-        encoder_motor_M3.set_power(50)
-        encoder_motor_M4.set_power(-50)
+        encode_fl.set_power(50)
+        encode_fr.set_power(-50)
+        encode_rl.set_power(50)
+        encode_rr.set_power(-50)
 
     def MoveRight():
         global auto_stage
-        encoder_motor_M1.set_power(50)
-        encoder_motor_M2.set_power(50)
-        encoder_motor_M3.set_power(-50)
-        encoder_motor_M4.set_power(-50)
+        encode_fl.set_power(50)
+        encode_fr.set_power(50)
+        encode_rl.set_power(-50)
+        encode_rr.set_power(-50)
 
     def MoveLeft():
         global auto_stage
-        encoder_motor_M1.set_power(-50)
-        encoder_motor_M2.set_power(-50)
-        encoder_motor_M3.set_power(50)
-        encoder_motor_M4.set_power(50)
+        encode_fl.set_power(-50)
+        encode_fr.set_power(-50)
+        encode_rl.set_power(50)
+        encode_rr.set_power(50)
 
     def StopMoving():
         global auto_stage
-        encoder_motor_M1.set_power(0)
-        encoder_motor_M2.set_power(0)
-        encoder_motor_M3.set_power(0)
-        encoder_motor_M4.set_power(0)
+        encode_fl.set_power(0)
+        encode_fr.set_power(0)
+        encode_rl.set_power(0)
+        encode_rr.set_power(0)
 
 
 auto_stage = 1
