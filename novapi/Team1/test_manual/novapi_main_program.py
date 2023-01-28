5cad87bf57da586a5235853e89bc4efc7ce5ae49
NETtoSan <netzamora05@gmail.com>
2023-01-29 01:58:48 +0700

    revert main program to old one

--- novapi/Team1/test_manual/novapi_main_program.py
+++ novapi/Team1/test_manual/novapi_main_program.py
@@ -6,27 +6,23 @@ from mbuild import gamepad
 import novapi
 import time
 
-# Important variables
-auto_stage = 0 # Auto
-shoot = 0 #
-invert = 0 # Invert movements
-feeddc = 1 # DC front on/off
-lrmode = 0  # 0 = shoot, 1 = arm
-turret_origin_angle = 0 # for reset servo angle
-bp = 100 # brushless power
-vl = 0.7 # Movement speed
+# initialize the variables
+auto_stage = 0
+shoot = 0
+invert = 0
+feeddc = 1
+lrmode = 0  # Differentiate between shoot and arm control mode
+bp = 50
+vl = 0.5
 
 # DC motors
-feeddc_main = "DC1" # Main feed belt
-feeddc_front = "DC2" # Front roller
-feeddc_aux = "DC3" # No need!
-unsighed_dc = "DC4" # Assign if new parts are added
-unsigned_dc2 = "DC5" # Assign if new parts are added
-handdc1 = "DC6" # hand updown 
-handdc2 = "DC7" # hand updown
-handdc_roll = "DC8" # Rotate cubes updown
- 
-# Indicators
+dc1_variable = "DC1"
+dc2_variable = "DC2"
+dc3_variable = "DC3"
+dc4_variable = "DC4"
+dc5_variable = "DC5"
+
+# Sensors
 dual_rgb_sensor_1 = dual_rgb_sensor_class("PORT2", "INDEX1")
 dual_rgb_sensor_2 = dual_rgb_sensor_class("PORT2", "INDEX2")
 
@@ -34,38 +30,20 @@ dual_rgb_sensor_2 = dual_rgb_sensor_class("PORT2", "INDEX2")
 smartservo_arm = smartservo_class("M6", "INDEX1")
 
 # Turret
-smartservo_pitch =   smartservo_class("M5", "INDEX1")
-smartservo_updown = smartservo_class("M5", "INDEX2")
+smartservo_updown = smartservo_class("M5", "INDEX1")
 
 # Bot motors
 encoder_motor_M1 = encoder_motor_class("M1", "INDEX1")
 encoder_motor_M2 = encoder_motor_class("M2", "INDEX1")
 encoder_motor_M3 = encoder_motor_class("M3", "INDEX1")
 encoder_motor_M4 = encoder_motor_class("M4", "INDEX1")
+encode_feed = encoder_motor_class("M5", "INDEX1")
 
 
 def AutoStart():
     global auto_stage
-    dual_rgb_sensor_1.set_led_color("blue")
-    while not gamepad.is_key_pressed("N3"):
-        if gamepad.is_key_pressed("N4"):
-            for i in range(3):
-                dual_rgb_sensor_1.set_led_color("red")
-                time.sleep(0.2)
-                dual_rgb_sensor_1.set_led_color("blue")
-                time.sleep(0.2)
-            AutoAssets.GrabCubeRoutine("right")
-        if gamepad.is_key_pressed("N1"):
-            for i in range(3):
-                dual_rgb_sensor_1.set_led_color("green")
-                time.sleep(0.2)
-                dual_rgb_sensor_1.set_led_color("red")
-                time.sleep(0.2)
-            AutoAssets.GrabCubeRoutine("left")           
-        pass
-        
-    Manual()
     pass
+
 
 def Manual():
     global auto_stage
@@ -77,35 +55,37 @@ def Manual():
     LoadMe()
     while True:
         time.sleep(0.001)
+        JoyRes.MovingJoystick(invert)
         ManualRes.InvertLED(invert)
         ManualRes.ControlLED(lrmode)
         JoyRes.MultiControl(lrmode, bp)
 
         if gamepad.is_key_pressed("Up"):
-            pass
+            ManualRes.MoveForward()
 
         if gamepad.is_key_pressed("Down"):
-            pass
+            ManualRes.MoveBackward()
 
         if gamepad.is_key_pressed("Left"):
-            power_expand_board.set_power(handdc_roll, 100)
+            ManualRes.MoveLeft()
 
-        elif gamepad.is_key_pressed("Right"):
-            power_expand_board.set_power(handdc_roll, -100)
-        else:
-            power_expand_board.stop(handdc_roll)
+        if gamepad.is_key_pressed("Right"):
+            ManualRes.MoveRight()
 
         if gamepad.is_key_pressed("N1"):
-            pass  
-        if gamepad.is_key_pressed("N4"):
-            pass
-
+            power_expand_board.set_power("DC5", -20)
+        elif gamepad.is_key_pressed("N4"):
+            power_expand_board.set_power("DC5", 20)
+        else:
+            power_expand_board.stop("DC5")
 
         if gamepad.is_key_pressed("N2"):
-            if bp == 100:
+            if bp == 50:
+                bp = 100
+            elif bp == 100:
                 bp = 0
             else:
-                bp = 100
+                bp = 50
             while not not gamepad.is_key_pressed("N2"):
                 pass
             pass
@@ -127,8 +107,8 @@ def Manual():
                 lrmode = 0
             while not not gamepad.is_key_pressed("R2"):
                 pass
-        
         # Control speed
+
         if gamepad.is_key_pressed("L_Thumb"):
             if vl == 0.5:
                 vl = 0.8
@@ -139,7 +119,6 @@ def Manual():
 
             while not not gamepad.is_key_pressed("L_Thumb"):
                 pass
-        
         # Invert control direction
         if gamepad.is_key_pressed("R_Thumb"):
             if invert == 0:
@@ -151,9 +130,9 @@ def Manual():
 
         # Dc feed
         if feeddc == 1:
-            power_expand_board.set_power(feeddc_front, 100)
+            power_expand_board.set_power(dc2_variable, -100)
         else:
-            power_expand_board.stop(feeddc_front)
+            power_expand_board.stop(dc2_variable)
 
 
 def LoadMe():
@@ -161,49 +140,62 @@ def LoadMe():
 
     smartservo_arm.set_power(50)
     smartservo_arm.move(30, 10)
+    # power_expand_board.set_power("BL1", 50)
+    # power_expand_board.set_power(dc1_variable, 50)
+    # power_expand_board.stop("BL1")
+    # power_expand_board.stop(dc1_variable)
 
 
 class JoyRes:
     def __init__(self):
         pass
 
-    def MovingShooting():
-        global vl
-        Lx = gamepad.get_joystick("Lx")
-        Ly = gamepad.get_joystick("Ly")
-        Rx = gamepad.get_joystick("Rx")
-
-        EFl = vl * (Ly - Rx)
-        EFr = vl * (-Lx - Rx)
-        ERl = -vl * (-Lx + Rx)
-        ERr = -vl * (Ly + Rx)
-
-        MovementAsset.move(EFl, EFr, ERl, ERr)
-
-        
-    def MovingArm():
-        global vl
-        Lx = gamepad.get_joystick("Lx")
-        Ly = gamepad.get_joystick("Ly")
-        Rx = gamepad.get_joystick("Rx")
-
-        EFl = -vl * (Lx + Rx)
-        EFr = -vl * (Ly + Rx)
-        ERl = vl * (Ly - Rx)
-        ERr = vl * (Lx - Rx)
-
-        MovementAsset.move(EFl, EFr, ERl, ERr)
-
-        
-    def TurretControl():
-        if gamepad.is_key_pressed("Up"):
-            smartservo_updown.move(10,10)
-
-        if gamepad.is_key_pressed("Down"):
-            smartservo_updown.move(-10,10)
-
-    def TurretControlOld():
+    def MovingJoystick(invert):
         global auto_stage
+        global vl
+        # Code for bot movement from left to right.
+        # If the bot starts slipping to unwanted direction. Tune variables here
+        
+        Lx = gamepad.get_joystick("Lx")  # literally Lx variable
+
+        Fl = Lx
+        Fr = Lx 
+        Rl = Lx 
+        Rr = Lx
+
+        EFl = vl * (gamepad.get_joystick("Ly") - Rl
+                    - gamepad.get_joystick("Rx"))
+        EFr = -vl * (gamepad.get_joystick("Ly") + Rr
+                     + gamepad.get_joystick("Rx"))
+        ERl = vl * (gamepad.get_joystick("Ly")
+                    + Fl - gamepad.get_joystick("Rx"))
+        ERr = -vl * (gamepad.get_joystick("Ly") - Fr
+                     + gamepad.get_joystick("Rx"))
+
+        if invert == 1:
+            # If the controls are inverted The arms are now the bot's front
+            EFr = vl * (gamepad.get_joystick("Ly")
+                        - Fl - gamepad.get_joystick("Rx"))
+            EFl = -vl * (gamepad.get_joystick("Ly") + Fr
+                         + gamepad.get_joystick("Rx"))
+            ERr = vl * (gamepad.get_joystick("Ly") + Rl
+                        - gamepad.get_joystick("Rx"))
+            ERl = -vl * (gamepad.get_joystick("Ly") - Rr
+                         + gamepad.get_joystick("Rx"))
+        encoder_motor_M1.set_power(EFl)
+        encoder_motor_M2.set_power(EFr)
+        encoder_motor_M3.set_power(ERl)
+        encoder_motor_M4.set_power(ERr)
+
+    def TurretControl():
+        global auto_stage
+        # > 40 < 60
+        # if smartservo_updown.get_value("angle") > -43:
+        #    smartservo_updown.move(-1, 10)
+
+        # if smartservo_updown.get_value("angle") < -60:
+        #    smartservo_updown.move(1, 10)
+        # else:
         if smartservo_updown.get_value("angle") < -56:
             servo_value = smartservo_updown.get_value("angle")
             while servo_value < -55:
@@ -216,20 +208,17 @@ class JoyRes:
 
     def FeedControl():
         if gamepad.is_key_pressed("L1"):
-            power_expand_board.set_power(feeddc_main, -100)
-            power_expand_board.set_power(feeddc_aux, 75)
+            encode_feed.set_power(-100)
         elif gamepad.is_key_pressed("L2"):
-            power_expand_board.set_power(feeddc_main, 100)
-            power_expand_board.set_power(feeddc_aux, -75)
+            encode_feed.set_power(100)
         else:
-            power_expand_board.stop("DC1")
-            power_expand_board.stop(feeddc_aux)
+            encode_feed.set_power(0)
 
     def ShootControl():
         if gamepad.is_key_pressed("R1"):
-            power_expand_board.set_power("DC5", -100)
+            power_expand_board.set_power(dc3_variable, -100)
         else:
-            power_expand_board.stop("DC5")
+            power_expand_board.stop(dc3_variable)
 
     def GrabControl():
         # DC4 L1 release R1 grab
@@ -243,18 +232,10 @@ class JoyRes:
         pass
 
     def HandControl():
-
-        power_expand_board.set_power(handdc1, -gamepad.get_joystick("Rx"))
-        power_expand_board.set_power(handdc2, -gamepad.get_joystick("Ry"))
-
-        power_expand_board.set_power("DC1",100) # It's all in ur head over time. And the similarities as you work with it
-        power_expand_board.set_power("DC1",100) # It's all in ur head over time. And the similarities as you work with it
-        
-        
-        if gamepad.is_key_pressed("Up"):
-            smartservo_pitch.move(-10,50)
-        if gamepad.is_key_pressed("Down"):
-            smartservo_pitch.move(10,50)
+        power_expand_board.set_power("DC6", gamepad.get_joystick("Ry"))
+        power_expand_board.set_power("DC7", gamepad.get_joystick("Ry"))
+        # smartservo_arm.move(gamepad.get_joystick("Ry"), 10)
+        pass
 
     def MultiControl(lc, bp):
         if lc == 0:
@@ -262,7 +243,7 @@ class JoyRes:
             JoyRes.TurretControl()
             JoyRes.ShootControl()
             JoyRes.FeedControl()
-            JoyRes.MovingShooting()
+
             # < 15 -- 23 > : 25 max
             power_expand_board.set_power("BL1", bp)
             power_expand_board.set_power("BL2", bp)
@@ -270,8 +251,6 @@ class JoyRes:
             # Hand control mode
             JoyRes.HandControl()
             JoyRes.GrabControl()
-            JoyRes.MovingArm()
-            power_expand_board.stop(feeddc_main)
             power_expand_board.stop("BL1")
             power_expand_board.stop("BL2")
 
@@ -279,8 +258,8 @@ class JoyRes:
 class ManualRes:
     def __init__(self):
         pass
-
     # Miscellaneous
+
     def InvertLED(i):
         if i != 0:
             dual_rgb_sensor_1.set_led_color("red")
@@ -292,152 +271,43 @@ class ManualRes:
             dual_rgb_sensor_2.set_led_color("red")
         else:
             dual_rgb_sensor_2.set_led_color("green")
+    # Joystick Controls
 
-class MovementAsset:
-    def __init__(self):
-        pass
+    def MoveBackward():
+        global auto_stage
+        encoder_motor_M1.set_power(-50)
+        encoder_motor_M2.set_power(50)
+        encoder_motor_M3.set_power(-50)
+        encoder_motor_M4.set_power(50)
 
-    def move(v1, v2, v3, v4):
-        encoder_motor_M1.set_power(v1)
-        encoder_motor_M2.set_power(v2)
-        encoder_motor_M3.set_power(v3)
-        encoder_motor_M4.set_power(v4)
+    def MoveForward():
+        global auto_stage
+        encoder_motor_M1.set_power(50)
+        encoder_motor_M2.set_power(-50)
+        encoder_motor_M3.set_power(50)
+        encoder_motor_M4.set_power(-50)
 
-    def stop():
+    def MoveRight():
+        global auto_stage
+        encoder_motor_M1.set_power(50)
+        encoder_motor_M2.set_power(50)
+        encoder_motor_M3.set_power(-50)
+        encoder_motor_M4.set_power(-50)
+
+    def MoveLeft():
+        global auto_stage
+        encoder_motor_M1.set_power(-50)
+        encoder_motor_M2.set_power(-50)
+        encoder_motor_M3.set_power(50)
+        encoder_motor_M4.set_power(50)
+
+    def StopMoving():
+        global auto_stage
         encoder_motor_M1.set_power(0)
         encoder_motor_M2.set_power(0)
         encoder_motor_M3.set_power(0)
         encoder_motor_M4.set_power(0)
 
-
-class AutoAssets:
-    def __init__(self):
-        pass
-
-    def MoveForward():
-        MovementAsset.move(70, 0, 0, -70)
-        pass
-
-    def MoveBackward():
-        MovementAsset.move(-70, 0, 0, 70)
-        pass
-
-    def RotateLeft():
-        MovementAsset.move(-70, -70, -70, -70)
-        pass
-
-    def RotateRight():
-        MovementAsset.move(70, 70, 70, 70)
-        pass
-    def StopMoving():
-        MovementAsset.move(0, 0, 0, 0)
-    def Shoot():
-        power_expand_board.set_power("DC3", 100)
-        pass
-
-    # Return value functions
-
-    def getSelfAngle():
-
-        angle = [novapi.get_pitch(), novapi.get_roll(), novapi.get_yaw()]
-        return angle
-
-
-    def GetDistance():
-        range = distance_sensor_1.get_distance()
-
-        return range
-
-    # Presets
-
-    def ShootRoutine():
-        dual_rgb_sensor_1.set_led_color("red")
-        dual_rgb_sensor_2.set_led_color("red")
-        time.sleep(1)    
-
-        power_expand_board.set_power("DC2",100)
-        power_expand_board.set_power("DC1",-100)
-        
-        AutoAssets.MoveForward()
-        time.sleep(0.25)
-        AutoAssets.RotateLeft()
-        time.sleep(0.25)
-
-        AutoAssets.MoveForward()
-        time.sleep(1)
-        AutoAssets.RotateRight()
-        time.sleep(0.2)
-        
-        AutoAssets.MoveForward()
-        power_expand_board.set_power("BL1",100)
-        power_expand_board.set_power("BL2",100)
-        time.sleep(0.75)
-
-        AutoAssets.StopMoving()
-        dual_rgb_sensor_2.set_led_color("green")
-
-        #time.sleep(5)
-        #for i in range(6):
-        #    dual_rgb_sensor_1.set_led_color("blue")
-        #    AutoAssets.RotateLeft()
-        #    time.sleep(0.1)
-        #    AutoAssets.StopMoving()
-        #    dual_rgb_sensor_1.set_led_color("red")
-        #    time.sleep(0.3)
-
-        pass
-
-    def EmbraceBallRoutine():
-
-        pass
-
-    def GrabCubeRoutine(sides): # THIS STARTS FROM THE LEFT FOR NOW
-        global feeddc_front
-        dual_rgb_sensor_1.set_led_color("green")
-        time.sleep(0.5)
-
-        # UTILIZE ARM
-        power_expand_board.set_power("DC4",-50)
-        time.sleep(0.7)
-        power_expand_board.set_power("DC4",0)
-        power_expand_board.set_power(handdc1, -100)
-        power_expand_board.set_power(handdc2, -100)
-        smartservo_pitch.move(-90,50)
-        time.sleep(1.9)
-        power_expand_board.stop(handdc1)
-        power_expand_board.stop(handdc2)
-
-        # TO THE CENTER
-        power_expand_board.set_power(feeddc_front,100)
-        power_expand_board.set_power(feeddc_main,-100)
-        AutoAssets.MoveForward()
-        time.sleep(0.26)
-        if sides is "left": 
-            AutoAssets.RotateRight()
-        elif sides is "right":
-            AutoAssets.RotateLeft()
-        time.sleep(0.25) #Rotate bot from the start
-        AutoAssets.MoveForward()
-        time.sleep(0.4452)
-        if sides is "left": 
-            AutoAssets.RotateLeft()
-        if sides is "right":
-            AutoAssets.RotateRight()
-        time.sleep(0.28)
-        AutoAssets.MoveForward()
-        time.sleep(1.6)
-
-        AutoAssets.StopMoving()     # Suppose this function stops all motors
-
-        AutoAssets.RotateRight()
-        time.sleep(0.7)
-        AutoAssets.StopMoving()
-        power_expand_board.set_power(handdc1, 100)
-        power_expand_board.set_power(handdc2, 100)
-        time.sleep(0.9)
-        power_expand_board.stop(handdc1)
-        power_expand_board.stop(handdc2)
-        pass
 
 auto_stage = 1
 while True:
