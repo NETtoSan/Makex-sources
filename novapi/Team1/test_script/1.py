c5c8b755c3f0d85178f47b214eb5374d0a83e618
NETtoSan <netzamora05@gmail.com>
2023-02-04 00:18:25 +0700

    add scripts

--- novapi/Team1/test_script/1.py
+++ novapi/Team1/test_script/1.py
@@ -3,7 +3,6 @@ import time
 # RESOURCES
 modes = ["cube", "test"]
 
-
 class led_lights():
     def __init__ (self):
         pass
@@ -11,17 +10,34 @@ class led_lights():
         color = ["red", "green", "blue"]
         try:
             color.index(col)
+            print(f"> Setting rgb color to {col}")
         except ValueError:
             print("> That color DOES NOT exist")
 
-class auto(object):
+class encode_motor():
+    def __init__ (self):
+        pass
+    def set_speed(speed):
+        print(f"setting motor at {speed}%")
+
+# BOT CORE
+class auto():
     def __init__ (self):
         self.lights = led_lights() #simulate_lights
 
     def modes(self,prompt):
         self.lights.set_rgb_color("cyan")
+        time.sleep(2)
+        print("Set motor speed to 50,-50,50,-50 (fwd) for 1 sec")
+        time.sleep(1)
+        print("Set motor speed to -50,50,-50,50 (rlf) for 1 sec")
+        time.sleep(1)
+
+        print("> Auto code done <\n")
+
+# FUNCTIONS
+print("----------- NETto!_NS Bootloader") 
 def ask():
-    print("\n----------- NETto!_NS Bootloader")
     prompt = input(f"Choose scenarios to test\n{' , '.join(modes)}\n")
     return prompt
 
