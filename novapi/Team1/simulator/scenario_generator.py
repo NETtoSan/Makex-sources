43b693342dce87c7259c07e894ab38eba876d5a9
NETtoSan <netzamora05@gmail.com>
2023-02-18 13:42:22 +0700

    i dont know what i did

---
+++ novapi/Team1/simulator/scenario_generator.py
@@ -0,0 +1,36 @@
+
+cube_select = 0
+cube_distance_between = 20
+cube_distance_arena = 120
+cube = [
+    "X", "M", "X", "A", "X", "K", "X", "E", "X"
+]
+
+def SelectCube(k):
+    global cube,cube_select
+    cube[cube_select] = f"{cube[cube_select].replace(' <', '')}"
+    cube[cube_select] = f"{cube[cube_select]} <"
+    if k == "d": cube[cube_select - 1] = f"{cube[cube_select - 1].replace(' <', '')}"
+    if k == "a": cube[cube_select + 1] = f"{cube[cube_select + 1].replace(' <', '')}"
+    
+def RenderCube():
+    print("-----------------------------------")
+    print(f"[{'] ['.join(cube)}]")
+    print("-----------------------------------")
+    
+
+
+def run_cube():
+    RenderCube()
+    print(f"\n\nDistance from starting area: {(cube_select + 1) * cube_distance_between} cm")
+
+SelectCube(None)
+RenderCube()
+while True:
+    keys = str(input("\nPress A , D to move a cube from left to right\n> ")).lower()
+    print(f"Key {keys} pressed" if keys == "a" or keys == "d" else "Enter key pressed " if keys == "" else "Invalid key")
+    cube_select = cube_select + 1 if keys == "d" else cube_select - 1 if keys == "a" else 0
+    SelectCube(keys)
+    if keys is not "": RenderCube()
+    if keys == "": run_cube()
+    
