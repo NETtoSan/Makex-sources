0021478059fa808533845ad48132c05efbbb9828
NETtoSan <netzamora05@gmail.com>
2023-06-22 03:57:01 +0700

    fix holonomic simulations

--- novapi/Team1/simulator/pure_persuit.py
+++ novapi/Team1/simulator/pure_persuit.py
@@ -1,3 +1,16 @@
+
+
+ ################################################
+ #         A HOLONOMIC MECANUM SOLUTIONS        # 
+ # - (C) 2023, NETtoSan                         #
+ # - Thai Nichi Institute of Technology         #
+ # This is to provide a solutions in which to   #
+ # drive mecanum wheel with holonomic aspects   #
+ # This is made suitable for odometry purposes  #
+ ################################################
+
+
+
 import math
 from time import sleep
 import random
@@ -25,11 +38,12 @@ def pure_persuit(target_pos):
     if keep_pos is True: starting_pos = [target_pos[0], target_pos[1]]
 
     # Holonomic code
-    power = (dX + dY) / 2 
+    power = (abs(dX) + abs(dY)) / 2 
     rot_speed = 0 # gamepad.get_joysticl("Rx")
 
     # This code calcultes time to run the motors
     duration = power # Dummy code
+
     '''
     # This code updates novapi location using novapi accelerometers
     novapi_x = 0
@@ -41,12 +55,34 @@ def pure_persuit(target_pos):
         sleep(0.01)
         print(f"{novapi_x}: {dX}, {novapi_y}: {dY}")
     '''
-    holonomic(power, [target_angle, dX, dY], rot_speed)
+
+    holonomic_angles(power, [target_angle, dX, dY], rot_speed)
+    holonomic_xy(power, [target_angle, dX, dY], rot_speed)
     #sleep(duration)
 
     #sleep(x)
 
-def holonomic(power, packet, rot_speed):
+def holonomic_angles(power:int, packet:list, rot_speed:int): # Use this for auto code!
+    #power = power/10
+    packet[0] = (packet[0] + 180) % 360 - 180
+    angle_rad = math.radians(- packet[0])
+
+    vx = round(power * math.cos(angle_rad))
+    vy = round(power * math.sin(angle_rad))
+
+    #if(isneg(packet[0])): vy = -vy
+    #if(isneg(packet[0]) or isneg(angle_rad)): vx = -vx
+
+    EFl =   constrain((vx - vy) - rot_speed, -100, 100)
+    EFr = - constrain((vx + vy) + rot_speed, -100, 100)
+    ERl =   constrain((vx + vy) - rot_speed, -100, 100)
+    ERr = - constrain((vx - vy) + rot_speed, -100, 100)
+
+    # --- OUTPUT --- #
+    print(f"------\n1) ACTUAL POWER ({packet[0]}) ({angle_rad})\n({vx}) ({vy})\nLEFT {EFl} : RIGHT {EFr}\nLEFT {ERl} : RIGHT {ERr}\n\n")
+    # --- OUTPUT --- #
+
+def holonomic_xy(power:int, packet:list, rot_speed:int): # Use this for manual code!
 
     # -- PACKET REQUIREMENTS -- #
     # packet[0] = target_angle
@@ -57,7 +93,6 @@ def holonomic(power, packet, rot_speed):
     lx = packet[1]        # gamepad.get_joystick("Lx")
     ly = packet[2]        # gamepad.get_joystick("Ly")
     rot_speed = rot_speed # gamepad.get_joystick("Rx")
-    angles = packet[0]    # default_angle, unused!
     theta = math.atan2(ly, lx)
     mPwr = constrain(math.hypot(lx, ly), -100, 100)
 
@@ -65,29 +100,25 @@ def holonomic(power, packet, rot_speed):
     cos = round(math.cos(theta - math.pi / 4))
     pmx = max(abs(sin), abs(cos))
 
-    EFl = mPwr * cos/pmx + rot_speed  # Motor front left
-    EFr = -mPwr * sin/pmx - rot_speed # Motor front right
-    ERl = mPwr * sin/pmx + rot_speed  # Motor back left
-    ERr = -mPwr * cos/pmx - rot_speed # Motor back right
+    EFl =   ((mPwr * cos/pmx) + rot_speed)  # Motor front left
+    EFr = - ((mPwr * sin/pmx) - rot_speed)  # Motor front right
+    ERl =   ((mPwr * sin/pmx) + rot_speed)  # Motor back left
+    ERr = - ((mPwr * cos/pmx) - rot_speed) # Motor back right
     
 
     # --- OUTPUT --- #
-    print(f"------\nACTUAL POWER\nLEFT {EFl} : RIGHT {EFr}\nLEFT {ERl} : RIGHT {ERr}\n--- OUTPUT CODE\nLEFT: {EFl} : RIGHT {-EFr}\nLEFT : {ERl} : RIGHT {-ERr}\n\n")
+    print(f"------\n2) ACTUAL POWER\nLEFT {EFl} : RIGHT {EFr}\nLEFT {ERl} : RIGHT {ERr}\n\n")
     # --- OUTPUT --- #
 
-    #print(f"OVERALL POWER\npure_persuit: {power} , holonomic: {mPwr}\nmax: {pmx}")
-    #print(f"{angles} : {total} : {total2}")
-    #print(f"{angles} : {theta}\n{angles} : {mPwr}\n{angles} : {sin}\n{angles} : {cos}\n{angles} : {pmx}")
-
-def isneg(v):
+def isneg(v:int):
     return False if v > 0 else False if v == 0 else True
 
-def constrain(v,mn,mx):
+def constrain(v:int, mn:int, mx:int):
     if v < mn : return mn
     if v > mx : return mx
     return v
 
-def run(x,y):
+def run(x:int ,y:int):
     global starting_pos, mode
     print("\n")
     pure_persuit([x,y])
--- novapi/Team1/test_manual/hawkeye_holonomic.py
+++ novapi/Team1/test_manual/hawkeye_holonomic.py
@@ -1,7 +1,15 @@
+
+ ################################################
+ #         A HOLONOMIC MECANUM SOLUTIONS        # 
+ # - (C) 2023, NETtoSan                         #
+ # - Thai Nichi Institute of Technology         #
+ # This is to provide a solutions in which to   #
+ # drive mecanum wheel with holonomic aspects   #
+ # This is made suitable for odometry purposes  #
+ ################################################
+
+
 from mbuild.encoder_motor import encoder_motor_class
-from mbuild.smartservo import smartservo_class
-from mbuild.dual_rgb_sensor import dual_rgb_sensor_class
-from mbuild import power_expand_board
 from mbuild import gamepad
 import math
 
@@ -10,41 +18,58 @@ encode_fr = encoder_motor_class("M2", "INDEX1")
 encode_rl = encoder_motor_class("M3", "INDEX1")
 encode_rr = encoder_motor_class("M4", "INDEX1")
 
-
-def holonomic(power, travelAngle, rotateRate):
-    pass
-
 # Keep bot position
-keep_pos  = False
 option = 1
+
 # Trigonometry
-def pure_persuit(target_pos):
+def pure_persuit():
     global starting_pos
     starting_angle = 90  # novapi.get_rot("Y")
 
-    dX = target_pos[0] - starting_pos[0]
-    dY = target_pos[1] - starting_pos[1]
-    path = int(math.sqrt((dX * dX) + (dY * dY)))
+    dX = gamepad.get_joystick("Lx")
+    dY = gamepad.get_joystick("Ly")
+    rX = gamepad.get_josytick("Rx")
 
     target_angle =  starting_angle - math.degrees(math.atan2(dY , dX)) if option == 1 else math.degrees(math.atan2(dY, dX))
-    angle_to_rotate = starting_angle + target_angle # Convert this to for loop using novapi accelerometer as reference
-    print(f"X:{dX} Y:{dY} ; PATH TO TRAVEL:{path} cm ; target_angle: {target_angle} ; bot angle then: {angle_to_rotate}")
+    power = (abs(dX) + abs(dY)) / 2
+    holonomic_xy(power, [target_angle, dX, dY], rX)
 
-    if keep_pos is True: starting_pos = [target_pos[0], target_pos[1]]
-    else: pass
+def holonomic_xy(power:int, packet:list, rot_speed:int): # Use this for manual code!
 
+    # -- PACKET REQUIREMENTS -- #
+    # packet[0] = target_angle
+    # packet[1] = x
+    # packet[2] = y
+    # -- PACKET REQUIREMENTS -- #
 
-def run(x,y):
-    global starting_pos, mode
-    print("\n")
-    pure_persuit([x,y])
-    print(f"Expected position: {starting_pos}")
-    print("-------")
+    lx = packet[1]        # gamepad.get_joystick("Lx") if not specified in pure_persuit
+    ly = packet[2]        # gamepad.get_joystick("Ly") if not specified in pure_persuit
+    rot_speed = rot_speed # gamepad.get_joystick("Rx") if not specified in pure_persuit
+
+    theta = math.atan2(ly, lx)
+    mPwr = constrain(math.hypot(lx, ly), -100, 100)
+
+    sin = round(math.sin(theta - math.pi / 4))
+    cos = round(math.cos(theta - math.pi / 4))
+    pmx = max(abs(sin), abs(cos))
+
+    EFl =   ((mPwr * cos/pmx) + rot_speed)  # Motor front left
+    EFr = - ((mPwr * sin/pmx) - rot_speed)  # Motor front right
+    ERl =   ((mPwr * sin/pmx) + rot_speed)  # Motor back left
+    ERr = - ((mPwr * cos/pmx) - rot_speed) # Motor back right
+
+    encode_fl.set_power(EFl)
+    encode_fr.set_power(EFr)
+    encode_rl.set_power(ERl)
+    encode_rr.set_power(ERr)
+
+def isneg(v:int):
+    return False if v > 0 else False if v == 0 else True
+
+def constrain(v:int, mn:int, mx:int):
+    if v < mn : return mn
+    if v > mx : return mx
+    return v
 
 while True:
-    try:
-        pos = input("Enter coordinates to simulate ")
-        pos = pos.split(" ")
-        run(int(pos[0]),int(pos[1]))
-    except SyntaxError:
-        print("Error")
+    pass
