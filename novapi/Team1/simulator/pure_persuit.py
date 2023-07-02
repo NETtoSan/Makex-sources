862f1b63f2025563a6a0dcfb86b7c5f6f2b0ea18
NETtoSan <netzamora05@gmail.com>
2023-07-02 13:17:42 +0700

    plotted automatic stage and tracking system

--- novapi/Team1/simulator/pure_persuit.py
+++ novapi/Team1/simulator/pure_persuit.py
@@ -21,7 +21,6 @@ from time import sleep
 keep_pos  = False
 wheel_size = 58 # In millimeters!. Will do it later
 starting_pos = [0,0] # Update this using novapi accelerometer (if present)
-mode = "2"
 
 def isneg(v:int):
     return False if v > 0 else False if v == 0 else True
@@ -35,23 +34,38 @@ def throttle_curve(v,s):
     return s * (v ** 2)
 
 # Pure persuit
-def pure_persuit(target_pos):
+def get_target(sA:int, dX:int, dY:int):
+    target_angle = sA - math.degrees(math.atan2(dY , dX))
+    return target_angle
+
+def pure_persuit(target_pos, auto):
     global starting_pos
     starting_angle = 90  # novapi.get_rot("Y")
     sensitivity = 0.005
 
-    dX = -1 * target_pos[0] # Change to gamepad Lx
-    dY = target_pos[1] # Change to gamepad Ly
+    x = target_pos[0] # Change to gamepad Lx
+    y = target_pos[1] # Change to gamepad Ly
+    dX = -1 * x
+    dY = y 
     rot_speed = 0
     power = constrain(throttle_curve(math.sqrt((dX * dX) + (dY * dY)), sensitivity), -100, 100)
     path = int(math.sqrt((dX * dX) + (dY * dY)))
 
-    target_angle =  starting_angle - math.degrees(math.atan2(dY , dX))
+    target_angle =  get_target(starting_angle, dX, dY)
     angle_to_rotate = starting_angle + target_angle # Convert this to for loop using novapi accelerometer as reference
     print(f"X:{dX} Y:{dY} ; PATH TO TRAVEL:{path} cm ; target_angle: {target_angle} ; bot angle then: {angle_to_rotate}")
 
     # Holonomic code
-    holonomic_angles(power, [target_angle, dX, dY], rot_speed)
+    if(auto == True):
+        while starting_pos[0] != x or starting_pos[1] != y:
+            
+            xX = starting_pos[0]
+            xY = starting_pos[0]
+            print(f"{starting_pos[0]} : {starting_pos[1]}")
+            #holonomic_angles(power, [target_angle, dX, dY], rot_speed)
+            pass
+    else:
+        holonomic_angles(power, [target_angle, dX, dY], rot_speed)
     #holonomic_xy(power, [target_angle, dX, dY], rot_speed)
 
 # Angle based holonomic
@@ -73,23 +87,23 @@ def holonomic_angles(power:int, packet:list, rot_speed:int): # Use this for auto
     ERr = - constrain((vx - vy) + rot_speed, -100, 100)
 
     # --- OUTPUT --- #
-    print(f"------\n1) ACTUAL POWER ({packet[0]}) ({angle_rad})\n({vx}) ({vy})\nLEFT {EFl} : RIGHT {-EFr}\nLEFT {ERl} : RIGHT {-ERr}\n\n")
+    print(f"({vx}) ({vy});   LEFT {EFl} : RIGHT {-EFr} LEFT {ERl} : RIGHT {-ERr}")
     # --- OUTPUT --- #
     
-def run(x:int ,y:int):
+def run(x:int ,y:int, auto:bool):
     global starting_pos, mode
-    pure_persuit([x,y])
+    pure_persuit([x,y], auto)
     print(f"Expected position: {starting_pos}")
     print("-------")
     if(keep_value == True):
         print("KEEP VALUE ON!")
 
-    os.system('cls')
-
 pos = [0, 0]
 keep_value = False
 
-while True:
+mode = "2" # 1 = WASD Manual mode ; 2 = Pure persuit Auto code
+# WASD Manual mode
+while mode == "1":
     try:
         if keyboard.is_pressed('w') or keyboard.is_pressed('a') or keyboard.is_pressed('s') or keyboard.is_pressed('d'):
             if keyboard.is_pressed('w'):
@@ -113,12 +127,22 @@ while True:
                 keep_value = False
 
         if keyboard.is_pressed("r"):
-            os.system('cls')
+            os.system('clear')
             print("# ---------- ! Reset ! ---------- #")
             pos = [0, 0]
             keep_value = False
             sleep(3)
 
         run(int(pos[0]),int(pos[1]))
+        os.system('cls')
+    except SyntaxError:
+        print("Error")
+
+# Pure persuit Auto mode 
+while mode == "2":
+    try:
+        pos = input("Enter coordinates to simulate ")
+        pos = pos.split(" ")
+        run(int(pos[0]),int(pos[1]), True)
     except SyntaxError:
         print("Error")
--- novapi/Team1/test_manual/hawkeye_holonomic.py
+++ novapi/Team1/test_manual/hawkeye_holonomic.py
@@ -8,7 +8,8 @@
  # drive mecanum wheel with holonomic aspects   #
  # This is made suitable for odometry purposes  #
  ################################################
-
+ 
+ # PS:1 This code is for MakeX Challenge 2023
 
 
 from mbuild.encoder_motor import encoder_motor_class
@@ -21,25 +22,28 @@ encode_fl = encoder_motor_class("M1", "INDEX1")
 encode_fr = encoder_motor_class("M2", "INDEX1")
 encode_rl = encoder_motor_class("M3", "INDEX1")
 encode_rr = encoder_motor_class("M4", "INDEX1")
-smart_cam = smart_camera_class("PORT1", "INDEX1")
 
-# Set smart camera mode to COLOR!
+# Test functions #
+smart_cam = smart_camera_class("PORT5", "INDEX1")
 smart_cam.set_mode("color")
+rot_spd = 0
+track = True
 
-rot_spd = int
-track = False
-
-# ---- PID ---- #
+# --- PID --- #
 kp = 0
 ki = 0
 kd = 0
 ks = 0
-# ---- PID ---- #
+# --- PID --- #
 
+novapi_travelled_x = 0
+novapi_travelled_y = 0
 
-# BACKGROUND PROCESS. PUT STUFFS IN IF YOU NEED TO RUN SOMETHING!
 def backgroundProcess():
-    lock_target(1) # track ball
+    track_while_scan.lock_target(1)
+    updatePosition()
+
+# Test functions #
 
 # Built functions to conpensate missing python functions
 def isneg(v):
@@ -49,134 +53,185 @@ def constrain(v, mn, mx):
     if v < mn : return mn
     if v > mx : return mx
     return v
-
-def drive(v1, v2, v3, v4):
-    encode_fl.set_power(v1)
-    encode_fr.set_power(v2)
-    encode_rl.set_power(v3)
-    encode_rr.set_power(v4)
-
-def throttle_curve(v,s):
-    return s * (v ** 2)
-
-
-# Find relative path
-def pure_pursuit(x, y, rot, auto:bool):
-    starting_angle = 90  # novapi.get_rot("Y") # Subtract starting angle. Which is exactly 90 degrees away.
-    sensitivity = 0.005  # 0 <----> 0.01 ; The less the wider, the less sensitive it will be
-                         # 0.01 = 1 : 1 input to power ratio. 
-                         # 0.00 = 0. No power will be provided. (value * power) ; power = 0, motor_power = 0
-
-    dX = -1 * x
-    dY = y
-    rX = rot
-
-    target_angle =  starting_angle - math.degrees(math.atan2(dY , dX))
-    power = constrain(throttle_curve(math.sqrt((dX * dX) + (dY * dY)), sensitivity) * 10, -100, 100)
-
-    novapi_x = 0 # Novapi accelerometer pointing RIGHT
-    novapi_y = 0 # Novapi accelerometer pointing FORWARD
-
-    if auto == True:
-        # Make this while to constantly update holonomic x and y values
-        while novapi_x != x and novapi_y != y:
-            holonomic(power, [target_angle, dX, dY], rX)
-    else:
-        holonomic(power, [target_angle, dX, dY], rX)
-
-# Calculate each motor power to travel
-def holonomic(power, packet, rot_speed): # Use this for auto code!
-
-    #power = power/10
-    packet[0] = (packet[0] + 180) % 360 - 180
-    angle_rad = math.radians(- packet[0])
-
-    vx = round(power * math.cos(angle_rad))
-    vy = round(power * math.sin(angle_rad))
-
-    EFl =   constrain((vx - vy) - rot_speed, -100, 100)
-    EFr = - constrain((vx + vy) + rot_speed, -100, 100)
-    ERl =   constrain((vx + vy) - rot_speed, -100, 100)
-    ERr = - constrain((vx - vy) + rot_speed, -100, 100)
-
-    drive(EFl, EFr, ERl, ERr)
-
-# --- TEST ---- #
-def lock_target(signature): # lock target -> find object -> track target
-    global rot_spd
-    if find_object(signature):
-        rot_spd = track_target(signature)
-    else:
-        rot_spd = 0
-
-def find_object(signature):
-    stat = bool
-    if not smart_cam: return False # Returns false if a camera is not detected
-
-    if smart_cam.detect_sign(signature): # Change this !. This is a theoretical code
-        stat = True
-    else:
-        stat = False
     
-    return stat
-
-def track_target(signature):
-    distance = float
-    distance = smart_cam.get_sign_x(signature) # Change this !. This is a theoretical code
-
-    # Ways to find PID for rotation speed!
-    distance = find_rot_pid(0, distance)                          # Change this if it doesnt work!
-    distance = smart_cam.get_sign_diff_speed(signature, "x", 160) # A makeblock's official way
-    return distance
-
-def find_rot_pid(s, t):
-
-    error = t - s
-    integral = integral + (error * 0.25)
-    derivative = error - prev_error
-    prev_error = error
-    start = (s - t)
-    tpower = (error * kp) + (integral*ki) + (derivative*kd)  + (start*ks)
-
-    # Limit values between -100 and 100
-    tpower = constrain(tpower, -100, 100)
-    return int(tpower)
-# ---- TEST ---- #
-
-def Manual():
-    backgroundProcess() # Run process in the background. 1st priority
-
-    x = gamepad.get_joystick("Rx"),
-    y = gamepad.get_joystick("Lx"),
-    rx = gamepad.get_joystick("Rx")
-
-    # Toggle target tracking mode
-    if gamepad.is_key_pressed("N1"):
-        if track == True:
-            track = False
+# Track while scan
+class track_while_scan:
+    def lock_target(signature):
+        global rot_spd
+        if smart_cam.detect_sign(signature):
+            rot_spd = ( smart_cam.get_sign_x(signature) - 160 ) * -0.5
         else:
-            track = True
-
-        # Prevent stuffs from happening!
-        while not not gamepad.is_key_pressed("N1"):
-            pass
-        pass
+            rot_spd = 0 
     
-    # Switch to target tracking mode to rotate bot
-    if track == True:
-        rx = rot_spd # The function is already called in backgroundProcess()
-    else:
-        rx = gamepad.get_joystick("Rx")
+    # An extra target lock using servos. While using camera to scan for objects
+    def find_target(signature):
+        pass
 
-    pure_pursuit(x, y, rx, False)
+    def find_target_x(signature):
+        pass
 
-def Auto():
-    pure_pursuit(0, 100, rot_spd, True)
-    pure_pursuit(300, 0, rot_spd, True)
+class motors:
+    # Calculate motor power using y = x theorem
+    def throttle_curve(v, s, e):
+        return s * (v ** e)
+    
+    # Drive all the motors in one go
+    def drive(v1, v2, v3, v4):
+        encode_fl.set_power(v1)
+        encode_fr.set_power(v2)
+        encode_rl.set_power(v3)
+        encode_rr.set_power(v4)
+        
+    # Find relative path
+    def pure_persuit(x, y, rot, auto:bool):
+        starting_angle = 90  # novapi.get_rot("Y")
+        dX = (-1 * x * 0.3)
+        dY = (y * 0.3)
+        rX = rot
 
-    Manual()
+        target_angle =  starting_angle - math.degrees(math.atan2(dY , dX))
+        power = constrain(motors.throttle_curve(math.sqrt((dX * dX) + (dY * dY)), 0.005, 2) * 10, -100, 100)
+        
+        # Automatic stage
+        if auto == True:
+            dX = (-1 * x * 0.3) - novapi_travelled_x
+            dY = (y * 0.3) - novapi_travelled_y
+            rX = 0
 
-while True:
-    #Manual()
-    #Auto()
-    pass
+            target_angle =  starting_angle - math.degrees(math.atan2(dY , dX))
+            power = constrain(motors.throttle_curve(math.sqrt((dX * dX) + (dY * dY)), 0.005, 2) * 10, -100, 100)
+
+            if novapi_travelled_x < x:
+                while novapi_travelled_x < x:
+                    novapi_travelled_x += novapi.get_acceleration("x")
+                    motors.holonomic(power, [target_angle, dX, dY], rx)
+            elif novapi_travelled_x > x:
+                while novapi_travelled_x > x:
+                    novapi_travelled_x += novapi.get_acceleration("x")
+                    motors.holonomic(power, [target_angle, dX, dY], rx)
+
+            motors.drive(0,0,0,0)
+        else:
+            motors.holonomic(power, [target_angle, dX, dY], rX)
+
+    # Calculate each motor power to travel
+    def holonomic(power, packet, rot_speed): # Use this for auto code!
+
+        #power = power/10
+        packet[0] = (packet[0] + 180) % 360 - 180
+        angle_rad = math.radians(- packet[0])
+
+        vx = round(power * math.cos(angle_rad))
+        vy = round(power * math.sin(angle_rad))
+
+        EFl =   constrain((vx - vy) - rot_speed, -100, 100)
+        EFr = - constrain((vx + vy) + rot_speed, -100, 100)
+        ERl =   constrain((vx + vy) - rot_speed, -100, 100)
+        ERr = - constrain((vx - vy) + rot_speed, -100, 100)
+
+        motors.drive(EFl, EFr, ERl, ERr)
+
+class dc_motor:
+    #ใส่ port ของ dc motor
+    #เช่น "DC1"
+    def __init__(self, port):
+        self.port = port #กำหนด port ของ dc motor
+    #กดเปิดปิด
+    mode_for_toggle = 0 #ตัวระบุตำแหน่งของสมาชิกใน list
+    def toggle(self, toggle_speed, toggle_key): #รับค่า ( ความเร็วมอเตอร์เป็น list สมาชิก 2ตัว,  ปุ่มที่ใช้กดเปิดปิด ) เช่น ([0, -100], "N1")
+        if gamepad.is_key_pressed(toggle_key): #กดเปิดปิดโดย สลับค่าตำแหน่งใน list
+            if self.mode_for_toggle == 0:
+                self.mode_for_toggle = 1
+            else:
+                self.mode_for_toggle = 0
+            while gamepad.is_key_pressed(toggle_key):
+                pass
+        else:
+            pass
+        power_expand_board.set_power(self.port, toggle_speed[self.mode_for_toggle]) #run มอเตอร์
+    #กดเปิดปิดเปลี่ยนทิศ
+    mode_for_double_toggle = {
+        "switch": 0,
+        "speed": 0
+    }
+    def double_toggle(self, double_toggle_speed, toggle_key_1, toggle_key_2):
+        if gamepad.is_key_pressed(toggle_key_1):
+            if self.mode_for_double_toggle["switch"] == 0:
+                self.mode_for_double_toggle["switch"] = 1
+            else:
+                self.mode_for_double_toggle["switch"] = 0
+            while gamepad.is_key_pressed(toggle_key_1):
+                pass
+        else:
+            pass
+        if gamepad.is_key_pressed(toggle_key_2):
+            if self.mode_for_double_toggle["speed"] == 0:
+                self.mode_for_double_toggle["speed"] = 1
+            else:
+                self.mode_for_double_toggle["speed"] = 0
+            while gamepad.is_key_pressed(toggle_key_2):
+                pass
+        else:
+            pass
+        if self.mode_for_double_toggle["switch"] == 1:
+            power_expand_board.set_power(self.port, double_toggle_speed[self.mode_for_double_toggle["speed"]])
+        else:
+            self.mode_for_double_toggle["speed"] = 0
+            power_expand_board.set_power(self.port, 0)
+    #กดค้างเปิดปิด
+    def hold(self, hold_default_speed, hold_speed, hold_key): #( ความเร็วเริ่มต้นของมอเตอร์, ความเร็วมอเตอร์เป็นตัวเลข ,  ปุ่มที่ใช้กดค้าง ) เช่น (0, 100, "N1")
+        if gamepad.is_key_pressed(hold_key): #กดค้าง
+            power_expand_board.set_power(self.port, hold_speed) #run ตอเมอเตอร์ 
+        else:
+            power_expand_board.set_power(self.port, hold_default_speed) #ค่า default ของมอเตอร์
+            pass
+    #กดค้างเปลี่ยนทิศ
+    def double_hold(self, double_hold_default_speed, double_speed, double_hold_key_1, double_hold_key_2): #( ความเร็วเริ่มต้นของมอเตอร์, ความเร็วมอเตอร์2ทิศเป็น list, ปุ่มที่ใช้กดค้างทิศที่1, ปุ่มที่ใช้กดค้างทิศที่2 ) เช่น (0, [-100, 100], "N1", "N2")
+        if gamepad.is_key_pressed(double_hold_key_1): #กดค้างหมุนทิศที่1
+            power_expand_board.set_power(self.port, double_speed[0])
+        elif gamepad.is_key_pressed(double_hold_key_2): #กดค้างหมุนทิศที่ 2
+            power_expand_board.set_power(self.port, double_speed[1])
+        else:
+            power_expand_board.set_power(self.port, double_hold_default_speed) #ค่า default ของมอเตอร์
+
+# Functions
+def updatePosition():
+    global novapi_travelled_x, novapi_travelled_y
+    novapi_travelled_x += novapi.get_acceleration("x")
+    novapi_travelled_y += novapi.get_acceleration("y")
+
+
+# Necessary for robot's functionality
+class challenge_default:
+    def __init__ (self):
+        self_mode = "default"
+    def auto(coords_list:list):
+
+        for coordinate in coords_list:
+            motors.pure_persuit(coordinate[0], coordinate[1], 0, True)
+
+    def manual():
+        while True:
+            global rot_spd, track
+            backgroundProcess()
+
+            x = gamepad.get_joystick("Lx")
+            y = gamepad.get_joystick("Ly")
+            rot = motors.throttle_curve(gamepad.get_joystick("Rx"), 0.0001, 3)
+
+            if gamepad.is_key_pressed("N1"):
+                if track == False:
+                    track = True
+                else:
+                    track = False
+                while not not gamepad.is_key_pressed("N1"):
+                    pass
+
+            if track == True:
+                rot = rot_spd
+
+
+        motors.pure_persuit(x, y, rot, False)
+
+
+challenge_default.manual()
