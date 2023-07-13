e428be60549b44296267023f618a310d2f722a9f
NETtoSan <netzamora05@gmail.com>
2023-07-13 23:51:52 +0700

    auto template

--- novapi/Team1/test_auto/auto_demo.py
+++ novapi/Team1/test_auto/auto_demo.py
@@ -1,11 +1,16 @@
-import novapi , time
 
-cube_distance = ranging_sensor_1.get_value()
 
-# BOT ---------> CUBE : 1cm
-while cube_distance > 3:
-    move(-50,50,50,-50)
-    cube_distance = ranging_sensor_1.get_value()
-
-move(0,0,0,0)
-grab_cube()
+ ################################################
+ #         A HOLONOMIC MECANUM SOLUTIONS        # 
+ # - (C) 2023, NETtoSan                         #
+ # - Thai Nichi Institute of Technology         #
+ # This is to provide a solution in which to    #
+ # drive mecanum wheel with holonomic aspects   #
+ # This is made suitable for odometry purposes  #
+ ################################################
+ 
+ # PS:1 This code DOES NOT provide full challenge bot capabilities
+ #      This only supplements necessary function to drive all 4 mecanum wheels
+ #      If you need full functionality, please use ./hawkeye_main_program.py
+ # PS:2 Rapid holonomic code change may occur. View latest holonomic simulation code via
+ #      ../simulator/{pid_test or pure_persuit}.py
